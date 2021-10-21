4958c59728dc914fabc4a24af1983051e5dfe183
Justin Freeman <justin@agileware.com.au>
2021-10-22 09:41:04 +1100

    Version 0.2.2

--- content-views-civicrm.php
+++ content-views-civicrm.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Content Views CiviCRM
  * Description: CiviCRM integraton for Content Views.
- * Version: 0.2.1
+ * Version: 0.2.2
  * Author: Agileware
  * Author URI: https://github.com/agileware/content-views-civicrm
  * Plugin URI: https://github.com/agileware/content-views-civicrm
@@ -18,7 +18,7 @@ class Content_Views_CiviCRM {
 	 * @since 0.1
 	 * @var string $version
 	 */
-	protected $version = '0.2.1';
+	protected $version = '0.2.2';
 
 	/**
 	 * Pro dependency.
